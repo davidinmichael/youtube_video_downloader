e6e46daedeafe06ae197e6f821f1d5dd5cd56b28
davidinmichael <davidinmichael@gmail.com>
2023-11-05 11:23:36 +0100

    I set up the templates for the frontend

--- api_downloader/urls.py
+++ api_downloader/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import *
 
+app_name = "api_downloader"
 
 urlpatterns = [
-    path("popular/", PopularVideos.as_view()),
-    path("search/", SearchVideos.as_view()),
-    path("video/details/<str:video_id>/", GetSingleVideo.as_view()),
-    path("channel/details/<str:channel_id>/", GetSingleChannel.as_view()),
-    path("download/<str:video_id>/", DownloadVideo.as_view()),
+    path("popular/", PopularVideos.as_view(), name="most_popular"),
+    path("search/", SearchVideos.as_view(), name="search_video"),
+    path("video/details/<str:video_id>/", GetSingleVideo.as_view(), name="video_details"),
+    path("channel/details/<str:channel_id>/", GetSingleChannel.as_view(), name="channel_details"),
+    path("download/<str:video_id>/", DownloadVideo.as_view(), name="download_video"),
 ]
