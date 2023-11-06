127ea9f42daf3275035141ab11452852a0755728
davidinmichael <davidinmichael@gmail.com>
2023-11-06 12:33:36 +0100

    Users can now search for their videos and download

--- downloader/urls.py
+++ downloader/urls.py
@@ -6,4 +6,6 @@ app_name = "downloader"
 urlpatterns = [
     path("", index, name="home"),
     path("video_details/", get_video, name="get_video"),
+    path("search/", search_videos, name="search_video"),
+    path("download/", download_videos, name="download"),
 ]
--- downloader/views.py
+++ downloader/views.py
@@ -1,6 +1,9 @@
-from multiprocessing import context
+
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from api_downloader.utils import *
 import requests
+
 
 def index(request):
     url = "http://127.0.0.1:8000/api/popular"
@@ -20,3 +23,25 @@ def get_video(request):
         "items": response["items"]
     }
     return render(request, "downloader/get-video.html", context)
+
+
+def search_videos(request):
+    if request.method == "POST":
+        search_keys = request.POST.get("search_keys")
+        response = search_video(search_keys)
+    else:
+        search_keys = request.GET.get("search_keys")
+        page_token = request.GET.get("page_token")
+        response = search_pagination(search_keys, page_token)
+    context = {
+        "data": response,
+        "items": response["items"],
+        "search_key": search_keys,
+    }
+    return render(request, "downloader/search.html", context)
+
+
+def download_videos(request):
+    video_id = request.GET.get("video_id")
+    download_video(video_id)
+    return HttpResponse(status=200)
